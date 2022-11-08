be3007fc498056dce737fb124c233d5c345edd94
Harry25R <hrob6771@uni.sydney.edu.au>
2022-11-08 22:52:38 +1100

    Added calculateCCA function

--- R/Processing_CPOP.R
+++ R/Processing_CPOP.R
@@ -1,27 +1,14 @@
 library(CCA)
 
 # Calculate the cononical correlation for a list of datasets
-CCA_Calculate <- function(dataset_list) {
-
-  # Create a list to store the canonical correlation
-  cca_list <- list()
-
-  # Loop through the list of datasets
-  for (i in seq_along(dataset_list)) {
-    # Loop through the list of datasets again
-    for (j in seq_along(dataset_list)) {
-      # If the datasets are different
-      if (i != j) {
-        # Calculate the canonical correlation
-        cca <- CCA::cc(dataset_list[[i]], dataset_list[[j]])
-
-        # Save the canonical correlation
-        cca_list[[paste(names(dataset_list)[i], names(dataset_list)[j], sep = "_")]] <-
-          cca$canon.cor
-      }
-    }
+calculateCCA <- function(x_list){
+  if(ncol(x_list[[1]]) > 1000){
+    warning("The number of features is > 1000, CCA will take a while to calculate.")
   }
-
-  # Return the list of canonical correlations
-  return(cca_list)
+  cor_mat <- sapply(seq_along(x_list), function(i){
+    sapply(seq_along(x_list), function(j){
+      CCA::cc(t(x_list[[i]]), t(x_list[[j]]))$cor[1]
+    })
+  })
+  return(cor_mat)
 }
