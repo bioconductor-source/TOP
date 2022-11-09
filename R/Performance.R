a5b245ff14282fd87f5dbe8babd5787aa50751e6
Nick <nickrrrr@gmail.com>
2022-11-09 16:01:05 +1100

    add documentation template

--- R/Performance.R
+++ R/Performance.R
@@ -1,14 +1,30 @@
-performance_cpop2 <- function(cpop_result, newx, covariates = NULL, s = "lambda.min"){
-
+#' @title FUNCTION_TITLE
+#' @description FUNCTION_DESCRIPTION
+#' @param cpop_result PARAM_DESCRIPTION
+#' @param newx PARAM_DESCRIPTION
+#' @param covariates PARAM_DESCRIPTION, Default: NULL
+#' @param s PARAM_DESCRIPTION, Default: 'lambda.min'
+#' @return OUTPUT_DESCRIPTION
+#' @details DETAILS
+#' @examples
+#'
+#' @rdname performance_cpop2
+#' @export
+#' @importFrom CPOP pairwise_col_diff
+#' @importFrom glmnet makeX
+#' @importFrom caret confusionMatrix
+#' @importFrom tibble enframe
+performance_cpop2 <- function(
+  cpop_result, newx, covariates = NULL, s = "lambda.min"
+) {
   # Go through and make predictions
-  newz = CPOP::pairwise_col_diff(newx)
+  newz <- CPOP::pairwise_col_diff(newx)
   if (!is.null(covariates)) {
     w3 <- glmnet::makeX(cbind(newz, covariates))
-    result_response = predict(object = cpop_result, newx = w3, s = s,
+    result_response <- predict(object = cpop_result, newx = w3, s = s,
                               type = "class")
-  }
-  else {
-    result_response = predict(object = cpop_result, newx = newz, s = s,
+  } else {
+    result_response <- predict(object = cpop_result, newx = newz, s = s,
                               type = "class")
   }
 
