c8b42b67c6d716ad4dc9f7268582f06608567897
Nick <nickrrrr@gmail.com>
2022-11-11 10:11:36 +1100

    add todo

--- R/Processing_CPOP.R
+++ R/Processing_CPOP.R
@@ -22,6 +22,7 @@ calculateCCA <- function(x_list) {
   if (ncol(x_list[[1]]) > 1000) {
     warning("The number of features is > 1000, CCA will take a while to calculate.")
   }
+  # TODO: this needs to be parallelised
   cor_mat <- sapply(seq_along(x_list), function(i){
     sapply(seq_along(x_list), function(j){
       CCA::cc(t(x_list[[i]]), t(x_list[[j]]))$cor[1]
